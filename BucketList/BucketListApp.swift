8e716a1ce96721d2f08170eb09e6e87b27d6eb67
abgbro <codepath@abhigarlapati.com>
2024-04-21 16:06:23 -0400

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by user256510 on 4/21/24.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
